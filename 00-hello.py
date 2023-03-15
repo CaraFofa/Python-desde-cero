eb46f604807b8be442a1d54856a233d834435d23
CaraFofa <127512829+CaraFofa@users.noreply.github.com>
2023-03-15 09:51:04 +0100

    Create 00-hello.py

---
+++ 00-hello.py
@@ -0,0 +1,8 @@
+# Esto es un comentario: Hola mundo
+print ("Hola mundo?")
+print ('Hola mundo')
+
+#Consultar el tipo de dato
+print (type('Hola mundo')) #tipo de dato string
+print (type(8)) #tipo de dato int sin comillas
+print (type(1.5)) #tipo de dato float
