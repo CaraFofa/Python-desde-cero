cebe72aef2ce21d6f29461606cdec41b416ecf86
CaraFofa <127512829+CaraFofa@users.noreply.github.com>
2023-03-15 09:51:55 +0100

    Create 01-variables.py

---
+++ 01-variables.py
@@ -0,0 +1,18 @@
+#Vamos a nombrar variables
+nombre = "Cristian string"
+edad = 37
+ciudad = "Valladolid"
+bull = True
+
+print (nombre)
+print (edad)
+print (ciudad)
+print (type(edad))
+print (type(bull))
+#Funciones del sistema
+print (len("nombre"))
+
+#Variable input
+minombre = input("¿De qué liga: ")
+print (minombre)
+print (type(minombre))
