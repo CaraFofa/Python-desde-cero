f2962f8f20eaad089fedf474666ce3d9d8b4a81b
CaraFofa <127512829+CaraFofa@users.noreply.github.com>
2023-03-15 09:53:23 +0100

    Create 02-operators.py

---
+++ 02-operators.py
@@ -0,0 +1,20 @@
+# Trabajando con operadores
+print (3+4)
+print (2-1)
+
+print ("Hola " + "Mundo")
+print ("Hola " * 5)
+print ("Hola")
+
+print (3==4)
+print ("aaa" < "aaa")
+
+# Operadores lógicos (Tablas de verdad, lógica Booleana)
+print (3 > 4 and "Hola" > "Python")
+print (3 > 4 or "Hola" > "Python")
+#print (3 > 4 not "Hola" > "Python") not opera distinto
+
+print (3 < 4 and "Hola" < "Python")
+print (3 < 4 or "Hola" < "Python")
+print (3 < 4 and "Hola" < "Python" and 4 == 4)
+print (not(3 > 4)) #Not False == True
