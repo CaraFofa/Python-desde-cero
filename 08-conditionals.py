5939fba6c0c0354631e626fc98fe223d577d19ba
CaraFofa <127512829+CaraFofa@users.noreply.github.com>
2023-03-15 10:01:56 +0100

    Create 08-conditionals.py

---
+++ 08-conditionals.py
@@ -0,0 +1,19 @@
+
+### Conditionals ###
+
+my_condition = False
+if my_condition:
+    print ("Se ejecuta la condición del if")
+
+my_condition = 5 * 2
+if my_condition >= 10:
+    print ("Se ejecuta la segunda condición")
+
+print ("La ejecución continúa")
+
+if my_condition > 10 and my_condition < 20:
+    print ("Es mayor que 10 y menor que 20")
+else:
+    print ("Es menor o igual que 10 o mayor o igual que 20")
+
+print ("La ejecución continua")
