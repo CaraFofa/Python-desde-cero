e4f251fbf51a8c1a1aa66adf276208265479cce2
CaraFofa <127512829+CaraFofa@users.noreply.github.com>
2023-03-17 16:58:49 +0100

    Create 10-functions.py

---
+++ 10-functions.py
@@ -0,0 +1,15 @@
+### Functions ###
+
+def my_function ():
+    print("Esto es una funcion")
+
+my_function ()
+my_function ()
+my_function ()
+
+def sum_two_valor (first_number, second_number): #La funcion consta de definir 2 "números",sumarlos y mostrarlos
+    print (first_number + second_number)
+
+sum_two_valor(5, 7)
+sum_two_valor(3.76, 4.25)
+sum_two_valor("Cris","tian")
